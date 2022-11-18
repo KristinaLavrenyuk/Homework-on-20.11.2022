45bcecde768614456f57ee2eaa01cc7cecfb9061
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-18 10:41:24 +0300

    Add files via upload

---
+++ Task 1(string).cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string n;
+    int a;
+    cin >> n;
+
+    a = n.length();
+    if (a % 2 == 0)
+    {
+        cout << n << endl;
+    }
+    cout << n;
+    system("pause");
+}
